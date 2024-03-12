5a49b68469ef7b2b42ba40be4d8f94d54a5e4386
ArchontisKostis <arxontisk02@gmail.com>
2024-03-12 10:14:12 +0200

    Update NewCountPage.jsx

--- src/pages/NewCountPage/NewCountPage.jsx
+++ src/pages/NewCountPage/NewCountPage.jsx
@@ -1,32 +1,66 @@
 import React, { useState, useEffect } from 'react';
-import { Col, Container, Row } from "react-bootstrap";
+import {Button, Col, Container, Modal, Row} from "react-bootstrap";
 import TileButton from "../../components/TileButton/TileButton.jsx";
 
 import './NewCountPage.css';
 
+const COUNTDOWN_SECONDS = 10;
+
 const NewCountPage = () => {
     const [count, setCount] = useState(0);
-    const [timer, setTimer] = useState(60);
+    const [timer, setTimer] = useState(COUNTDOWN_SECONDS);
+    const [today, setToday] = useState(new Date());
+    const [startedCounting, setStartedCounting] = useState(false);
 
-    const handleClick = () => {
+    const handleCountClick = () => {
         setCount(count + 1);
+        setStartedCounting(true);
     };
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleSave = () => {
+        // Get the current date and time
+        const now = new Date();
+        setToday(now);
+
+        console.log("Ημερομηνία: " + now);
+        console.log("Αριθμός αναπνοών: " + count);
+
+        // Reset the counter and timer
+        setCount(0);
+        setTimer(COUNTDOWN_SECONDS);
+
+        // Close the modal
+        handleClose();
+    }
+
     useEffect(() => {
-        const intervalId = setInterval(() => {
-            // Decrease timer by 1 every second
-            setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : 0));
+        if (startedCounting) {
+            const intervalId = setInterval(() => {
+                // Decrease timer by 1 every second
+                setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : 0));
 
-            // Show alert when the countdown stops
-            if (timer === 0) {
-                clearInterval(intervalId);
-                alert(`Countdown stopped! Final count: ${count}`);
-            }
-        }, 1000);
+                // Show alert when the countdown stops
+                if (timer < 1) {
+                    clearInterval(intervalId);
 
-        // Clear interval when component is unmounted
-        return () => clearInterval(intervalId);
-    }, [count, timer]); // Include count and timer in the dependency array
+                    // Reset flags
+                    setCount(0);
+                    setTimer(COUNTDOWN_SECONDS);
+                    setStartedCounting(false)
+
+                    handleShow();
+                }
+            }, 1000);
+
+            // Clear interval when component is unmounted
+            return () => clearInterval(intervalId);
+        }
+    }, [startedCounting]); // Include count and timer in the dependency array
 
     return (
         <div className="new-count-page">
@@ -35,12 +69,34 @@ const NewCountPage = () => {
                 {timer}
             </p>
             <div className="breath-btn-container">
-                <button className="heart-button" onClick={handleClick}>
+                <button className="heart-button" onClick={handleCountClick}>
                     <i className="bi bi-lungs-fill">
                         <p>{count}</p>
                     </i>
                 </button>
             </div>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Τέλος Χρονομέτρησης</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Η χρονομέτρηση τελείωσε. <br/>
+                    Ο αριθμός των αναπνοών του κατοικίδιου είναι: <b>{count}</b>.
+                    <br/> <br/>
+                    <i>
+                        Επιλέξτε "Αποθήκευση" για να αποθηκεύσετε την μέτρηση ή "Ακύρωση" για να την ακυρώσετε.
+                    </i>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Ακύρωση
+                    </Button>
+                    <Button variant="success" onClick={handleSave}>
+                        Αποθήκευση
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
